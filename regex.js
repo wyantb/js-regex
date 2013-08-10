ffcb0c6314d61754d4509275ae4f16dbe48f946a
Brian Wyant <wyantb@gmail.com>
2013-08-09 21:20:04 -0400

    Track pieces as they're added in _last

--- regex.js
+++ regex.js
@@ -10,26 +10,47 @@
         this._current = '';
         this._cache = {};
         this._flags = undefined;
+        this._last = '';
     };
 
     Regex.constructor = Regex;
     Regex.fn = Regex.prototype;
 
     Regex.fn.literal = function literal(character) {
-        this._current += getLiteral(character);
+        this._current += this._last;
+        this._last = getLiteral(character);
+        return this;
+    };
+
+    Regex.fn.literals = function literals(string) {
+        this._current += this._last;
+        this._last = getLiterals(string);
         return this;
     };
 
     Regex.fn.startGroup = function startGroup() {
+        this._current += this._last;
+        this._last = '';
+
         return new Group(this);
     };
 
     Regex.fn.test = function test(string) {
+        this._current += this._last;
+        this._last = '';
+
         var regex = this._cache[this._current];
-        if (regex) {
+        if (!regex) {
             regex = this._cache[this._current] = new RegExp(this._current, this._flags);
         }
         return regex.test(string);
+    };
+
+    Regex.fn._purge = function _purge() {
+        this._current += this._last;
+        this._last = '';
+
+        return this;
     };
 
     Regex.fn.reset = function() {
@@ -52,6 +73,28 @@
         return specialCharacters.indexOf(character) === -1 ? character : '\\' + character;
     }
 
+    function getLiterals(string) {
+        var literals = '';
+        for (var i = 0, len = string.length; i < len; i++) {
+            literals += getLiteral(string[i]);
+        }
+        return literals;
+    }
+
+    function addToNode(node, content) {
+        if (typeof node._current === 'string') {
+            node._last = content;
+            return;
+        }
+        else if (typeof node._contents === 'string') {
+            node._last = content;
+            return;
+        }
+
+        // should never happen in prod
+        throw new Error('unable to add to parent node?');
+    }
+
     // -----------------------
     // Group objects
     // -----------------------
@@ -61,18 +104,35 @@
         this._header = '(?:'; // non-capturing by default
         this._contents = '';
         this._footer = ')';
+        this._last = '';
     };
 
     Group.constructor = Group;
     Group.fn = Group.prototype;
 
     Group.fn.literal = function literal(character) {
-        this._contents += getLiteral(character);
+        this._contents += this._last;
+        this._last = getLiteral(character);
         return this;
     };
 
+    Group.fn.literals = function literals(string) {
+        this._contents += this._last;
+        this._last = getLiterals(string);
+        return this;
+    };
+
+    Group.fn.startGroup = function startGroup() {
+        this._current += this._last;
+        this._last = '';
+
+        return new Group(this);
+    };
+
     Group.fn.closeGroup = function closeGroup() {
-        this._parent.add(this._header + this._contents + this._footer);
+        this._contents += this._last;
+        this._last = '';
+        addToNode(this._parent, this._header + this._contents + this._footer);
         return this._parent;
     };
 
